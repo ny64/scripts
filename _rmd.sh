2e5431102fcab601e28f89bd069c660e39abdf00
Peter Breitzler <peter.breitzler@continental-corporation.com>
2023-07-18 13:09:34 +0200

    Added extension option

--- _rmd.sh
+++ _rmd.sh
@@ -1,37 +1,46 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Script: PDF Duplicate Remover
+# Script: Duplicate PDF Remover
 #
-# Description: This script finds duplicate PDF files in a specified directory 
-#              or the current directory, if no directory is specified.
+# Description: This script finds duplicate PDF files or files of another specified extension 
+#              in a given directory or the current directory if no directory is specified.
 #              Duplicates are determined by a naming scheme where duplicates
-#              of a file "file.pdf" are named as "file (1).pdf", "file (2).pdf", etc.
+#              of a file "file.ext" are named as "file (1).ext", "file (2).ext", etc.
 #              Among the duplicates, the script renames the file with the highest count
-#              to the original name (e.g., "file.pdf") and removes all other duplicates.
+#              to the original name (e.g., "file.ext") and removes all other duplicates.
 #
-# Usage: ./script.sh [dir]
-#        where [dir] is an optional argument specifying the directory to clean up.
+# Usage: ./script.sh -e [extension] [dir]
+#        where [extension] is an optional argument specifying the file extension to look for
+#        (without the leading dot). If no extension is specified, "pdf" is used.
+#        [dir] is an optional argument specifying the directory to clean up.
 #        If [dir] is not provided, the script operates on the current directory.
 #
 # Note: Please ensure you have a backup of your files before running the script. 
 #       This script will remove files and there's no simple way to recover them.
 #
-# Author: Peter Breitzler 
+# Author: Peter Breitzler
 # Date: 18. Jul 2023
+
+extension="pdf"
+dir="./"
+
+while getopts e: option
+do
+case "${option}"
+in
+e) extension=${OPTARG};;
+esac
+done
+shift $((OPTIND -1))
 
 if [ "$1" ]; then
   dir="$1"
-else
-  dir="./"
-fi cd "$dir" || exit
+fi
 
-# Iterate over each unique basename of files
-for file in $(ls | grep -Eo '.*\s\(\d+\)\.pdf' | sed -E 's/ \([0-9]+\)\.pdf//g' | sort | uniq); do
-  # Find the file with the highest count
-  highest_count=$(ls "${file}"\ \(*.pdf | grep -Eo '\([0-9]+\)\.pdf' | sed -E 's/\(([^)]+)\)\.pdf/\1/g' | sort -nr | head -n1)
-  # Rename the file with the highest count to the original name
-  mv -v "${file} (${highest_count}).pdf" "${file}.pdf"
-  # Remove all other duplicates
-  rm -v "${file}"\ \(*\).pdf
+cd "$dir" || exit
+
+for file in $(ls | grep -Eo '.*\s\(\d+\)\.'"$extension" | sed -E 's/ \([0-9]+\)\.'"$extension"'//g' | sort | uniq); do
+  highest_count=$(ls "${file}"\ \(*."$extension" | grep -Eo '\([0-9]+\)\.'"$extension" | sed -E 's/\(([^)]+)\)\.'"$extension"'/\1/g' | sort -nr | head -n1)
+  mv -v "${file} (${highest_count}).${extension}" "${file}.${extension}"
+  rm -v "${file}"\ \([0-9]*\)."${extension}"
 done
-
